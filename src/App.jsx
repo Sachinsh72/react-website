9d16fb0ac2ac9c0c4cc35db7a9114a5bb990d93c
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-10-13 21:05:39 +0530

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
 
 function App() {
 
   return (
     <>
-      <h1 className='text-center bg-purple-600 text-yellow-400'>React Website</h1>
+      <Header/>
+      <Home/>
+      <Footer/>
     </>
   )
 }
